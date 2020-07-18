11a206e7d4f2701fdafa5322e157a854f005a898
Mauri Mello <mauricorreademello@gmail.com>
2020-07-18 20:48:13 -0300

    Resolvi o conflito de Olas

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 	echo "Olá Mundo 456!";
 
-	echo "Olá Udemy";
-=======
-	echo "Olá Mundo123!";
-
-	echo "Olá hcode!";
->>>>>>> ed871e2652f81baba6de0fefc9559e5ab999c840
-
+	echo "Olá Udemy e Hcode!";
 ?>
